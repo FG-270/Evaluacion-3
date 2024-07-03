4792ff94491865b180c3417cb5109ffe771eb523
Felipe Galdames Sazie <feli.galdames@duocuc.cl>
2024-07-03 11:45:52 -0400

    primer commit

---
+++ main.py
@@ -0,0 +1,44 @@
+'''
+Gestión de Biblioteca: Desarrolla una aplicación en Python que permita gestionar una biblioteca. La aplicación debe
+cumplir con las siguientes funcionalidades que debe proveer al usuario través de un menú de opciones:
+Registrar libros: Solicita al usuario ingresar el título, autor y género de un libro. Almacena los datos en una colección,
+por ejemplo, en un diccionario.
+Buscar libros por autor: Permite al usuario buscar libros por el nombre del autor.
+Mostrar lista de libros: Muestra en pantalla la lista completa de libros registrados.
+Salir del programa.
+
+Recuerda: implementar cada funcionalidad en funciones separadas y permitir que el programa funcione hasta que el
+usuario decida salir. Que los datos de la aplicación persistan entre distintas ejecuciones de este, almacenando y
+recuperando sus datos en algún tipo de archivo de texto. Y además que use al menos una librería estándar de
+Python.
+
+'''
+
+
+'''
+1) Menu principal con opciones
+2) Registrar libros
+    ingresar: titulo, autor, genero
+3) BUscar libros por autor
+4) mostrar libros
+'''
+
+
+def cargar_biblioteca():
+    return
+
+def guardar_biblioteca():
+    return
+
+def registrar_libro():
+    return
+
+def buscar_libro():
+    return
+
+
+
+
+
+while True:
+    print('Menu:')
